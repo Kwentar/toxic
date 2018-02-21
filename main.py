8c75742810cc67e9f3bf80d065d0614ab5c03dea
Aleksei Alekseev <alekseev.yeskela@gmail.com>
2018-02-21 03:17:47 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,231 @@
+import gensim
+import tensorflow as tf
+import pandas as pd
+import nltk
+import string
+import itertools
+import re
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from string import punctuation
+import numpy as np
+from tqdm import tqdm
+
+from word2vec import clean_data
+
+stop_words = set(stopwords.words('english'))
+from keras.models import Model
+from keras.layers import Dense, Embedding, Input
+from keras.layers import LSTM, Bidirectional, GlobalMaxPool1D, Dropout
+from keras.preprocessing import text, sequence
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+
+
+
+def preprocessing_text(lst_with_text):
+    lst_new = []
+    available_symbols = 'qwertyuiopasdfghjklzxcvbnm.?!"\' -'
+    replaced = set()
+    for el in lst_with_text:
+        tmp_str = el.lower()
+        tmp_str2 = tmp_str
+        for sym in tmp_str:
+            if sym not in available_symbols:
+                tmp_str2 = tmp_str2.replace(sym, ' ')
+                replaced.add(sym)
+        lst_new.append(tmp_str2)
+    return lst_new
+'''
+df = pd.read_csv('train.csv')
+print(df.head())
+column = 'toxic'
+
+filtered_df = df.filter(items=['id', 'comment_text', column])
+true_value = filtered_df[filtered_df[column] == 1]
+false_value = filtered_df[filtered_df[column] == 0]
+lst_true = true_value['comment_text'].tolist()
+lst_false = false_value['comment_text'].tolist()
+print(filtered_df[filtered_df[column] == 1].head())
+print(true_value.shape, false_value.shape, filtered_df.shape)
+
+for el in lst_true[:2]:
+    print(el, clean_data(el))'''
+
+def aaaa():
+    # This Python 3 environment comes with many helpful analytics libraries installed
+    # It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
+    # For example, here's several helpful packages to load in
+
+    import numpy as np # linear algebra
+    import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+    # Input data files are available in the "../input/" directory.
+    # For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+    from subprocess import check_output
+
+
+    # Any results you write to the current directory are saved as output.
+
+    from keras.models import Model
+    from keras.layers import Dense, Embedding, Input
+    from keras.layers import LSTM, Bidirectional, GlobalMaxPool1D, Dropout
+    from keras.preprocessing import text, sequence
+    from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+    max_features = 20000
+    maxlen = 300
+
+
+    train = pd.read_csv("train.csv")
+    test = pd.read_csv("test.csv")
+    train = train.sample(frac=1)
+
+    list_sentences_train = train["comment_text"].fillna("CVxTz").values
+    list_classes = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
+    y = train[list_classes].values
+    list_sentences_test = test["comment_text"].fillna("CVxTz").values
+
+
+    tokenizer = text.Tokenizer(num_words=max_features)
+    tokenizer.fit_on_texts(list(list_sentences_train))
+    list_tokenized_train = tokenizer.texts_to_sequences(list_sentences_train)
+    list_tokenized_test = tokenizer.texts_to_sequences(list_sentences_test)
+
+    test = text.text_to_word_sequence(list_sentences_train)
+    X_t = sequence.pad_sequences(list_tokenized_train, maxlen=maxlen)
+    X_te = sequence.pad_sequences(list_tokenized_test, maxlen=maxlen)
+
+    def get_model():
+        embed_size = 256
+        inp = Input(shape=(300, ))
+        x = Bidirectional(LSTM(50, return_sequences=True))(inp)
+        x = GlobalMaxPool1D()(x)
+        x = Dropout(0.4)(x)
+        x = Dense(50, activation="relu")(x)
+        x = Dropout(0.1)(x)
+        x = Dense(6, activation="sigmoid")(x)
+        model = Model(inputs=inp, outputs=x)
+        model.compile(loss='binary_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
+
+        return model
+
+
+    model = get_model()
+    batch_size = 32
+    epochs = 20
+
+
+    file_path="weights_base.best.hdf5"
+    checkpoint = ModelCheckpoint(file_path, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+
+    early = EarlyStopping(monitor="val_loss", mode="min", patience=20)
+
+
+    callbacks_list = [checkpoint, early] #early
+    #model.fit(X_t, y, batch_size=batch_size, epochs=epochs, validation_split=0.1, callbacks=callbacks_list)
+
+    model.load_weights(file_path)
+
+    y_test = model.predict(X_te)
+
+
+
+    sample_submission = pd.read_csv("sample_submission.csv")
+
+    sample_submission[list_classes] = y_test
+
+    sample_submission.to_csv("baseline.csv", index=False)
+    #lst_true = preprocessing_text(lst_true)
+    #lst_false = preprocessing_text(lst_false)
+
+
+def convert_sentence_to_indexes(text, inverse_index_dict, word2vec_model, max_len):
+    filtered = [x for x in clean_data(text).split() if x]
+
+    res = [inverse_index_dict[word] for word in filtered if word in word2vec_model.wv.vocab]
+    if len(res) > max_len:
+        res = res[:max_len]
+    elif len(res) < max_len:
+        res = np.concatenate((res, np.array([len(word2vec.wv.vocab)] * (max_len - len(res)))))
+    return res
+
+
+def make_index(word2vec_model):
+    index_dict = dict()
+    inverse_index_dict = dict()
+    index = 0
+    for word in tqdm(word2vec_model.wv.vocab, desc='create index'):
+        index_dict[index] = word
+        inverse_index_dict[word] = index
+        index += 1
+    return index_dict, inverse_index_dict
+
+
+def get_model(vector_size, word_count, max_sequence_length, embedding_weights):
+    inp = Input(shape=(max_sequence_length,))
+    embedding_layer = Embedding(word_count+1,
+                                vector_size,
+                                weights=[embedding_weights],
+                                input_length=max_sequence_length,
+                                trainable=False)(inp)
+    x = Bidirectional(LSTM(100, return_sequences=True))(embedding_layer)
+    x = GlobalMaxPool1D()(x)
+    x = Dropout(0.4)(x)
+    x = Dense(50, activation="relu")(x)
+    x = Dropout(0.1)(x)
+    x = Dense(6, activation="sigmoid")(x)
+    model = Model(inputs=inp, outputs=x)
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    return model
+
+
+train = pd.read_csv("train.csv")
+
+train = train.sample(frac=1)
+
+list_sentences_train = train["comment_text"].fillna("CVxTz").values
+list_classes = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
+y = train[list_classes].values
+
+word2vec = gensim.models.Word2Vec.load('w2v.model')
+
+forward_index, invert_index = make_index(word2vec)
+
+
+train_sentences = list()
+max_len = 300
+for sentence in tqdm(list_sentences_train, desc='normalize sentences'):
+    norm_sentence = convert_sentence_to_indexes("make ready do", invert_index, word2vec, max_len)
+
+    train_sentences.append(norm_sentence)
+train_sentences = np.array(train_sentences)
+
+
+embedding_matrix = np.zeros((len(word2vec.wv.vocab)+1, word2vec.vector_size))
+for word, i in tqdm(invert_index.items(), desc='create embedding weights'):
+    if word in word2vec.wv.vocab:
+        embedding_matrix[i] = word2vec.wv[word]
+
+print('Null word embeddings: %d' % np.sum(np.sum(embedding_matrix, axis=1) == 0))
+
+
+model = get_model(word2vec.vector_size, len(word2vec.wv.vocab), max_len, embedding_matrix)
+
+batch_size = 32
+epochs = 20
+
+file_path = "weights_base.best.hdf5"
+checkpoint = ModelCheckpoint(file_path, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+
+early = EarlyStopping(monitor="val_loss", mode="min", patience=20)
+
+callbacks_list = [checkpoint, early]  # early
+model.fit(train_sentences, y, batch_size=batch_size, epochs=epochs, validation_split=0.1, callbacks=callbacks_list)
+
---
+++ word2vec.py
@@ -0,0 +1,138 @@
+import itertools
+import re
+import numpy as np
+from gensim.models import KeyedVectors
+from nltk import WordNetLemmatizer
+from nltk.corpus import stopwords
+import gensim
+import pandas as pd
+from tqdm import tqdm
+
+stop_words = set(stopwords.words('english'))
+
+
+def clean_data(text, lowercase=True, remove_stops=True):
+    txt = text
+
+    # Replace apostrophes with standard lexicons
+    txt = txt.replace("isn't", "is not")
+    txt = txt.replace("aren't", "are not")
+    txt = txt.replace("ain't", "am not")
+    txt = txt.replace("won't", "will not")
+    txt = txt.replace("didn't", "did not")
+    txt = txt.replace("shan't", "shall not")
+    txt = txt.replace("haven't", "have not")
+    txt = txt.replace("hadn't", "had not")
+    txt = txt.replace("hasn't", "has not")
+    txt = txt.replace("don't", "do not")
+    txt = txt.replace("wasn't", "was not")
+    txt = txt.replace("weren't", "were not")
+    txt = txt.replace("doesn't", "does not")
+    txt = txt.replace("'s", " is")
+    txt = txt.replace("'re", " are")
+    txt = txt.replace("'m", " am")
+    txt = txt.replace("'d", " would")
+    txt = txt.replace("'ll", " will")
+    txt = txt.replace("--th", " ")
+
+    # More cleaning
+    txt = re.sub(r"alot", "a lot", txt)
+    txt = re.sub(r"what's", "", txt)
+    txt = re.sub(r"What's", "", txt)
+    txt = re.sub(r"\'s", " ", txt)
+    txt = txt.replace("pic", "picture")
+    txt = re.sub(r"\'ve", " have ", txt)
+    txt = re.sub(r"can't", "cannot ", txt)
+    txt = re.sub(r"n't", " not ", txt)
+    txt = re.sub(r"I'm", "I am", txt)
+    txt = re.sub(r" m ", " am ", txt)
+    txt = re.sub(r"\'re", " are ", txt)
+    txt = re.sub(r"\'d", " would ", txt)
+    txt = re.sub(r"\'ll", " will ", txt)
+    txt = re.sub(r"60k", " 60000 ", txt)
+    txt = re.sub(r" e g ", " eg ", txt)
+    txt = re.sub(r" b g ", " bg ", txt)
+    txt = re.sub(r"\0s", "0", txt)
+    txt = re.sub(r" 9 11 ", "911", txt)
+    txt = re.sub(r"e-mail", "email", txt)
+    txt = re.sub(r"\s{2,}", " ", txt)
+    txt = re.sub(r"quikly", "quickly", txt)
+    txt = re.sub(r"imrovement", "improvement", txt)
+    txt = re.sub(r"intially", "initially", txt)
+    txt = re.sub(r"quora", "Quora", txt)
+    txt = re.sub(r" dms ", "direct messages ", txt)
+    txt = re.sub(r"demonitization", "demonetization", txt)
+    txt = re.sub(r"actived", "active", txt)
+    txt = re.sub(r"kms", " kilometers ", txt)
+    txt = re.sub(r"KMs", " kilometers ", txt)
+    txt = re.sub(r" cs ", " computer science ", txt)
+    txt = re.sub(r" upvotes ", " up votes ", txt)
+    txt = re.sub(r" iPhone ", " phone ", txt)
+    txt = re.sub(r"\0rs ", " rs ", txt)
+    txt = re.sub(r"calender", "calendar", txt)
+    txt = re.sub(r"ios", "operating system", txt)
+    txt = re.sub(r"gps", "GPS", txt)
+    txt = re.sub(r"gst", "GST", txt)
+    txt = re.sub(r"programing", "programming", txt)
+    txt = re.sub(r"bestfriend", "best friend", txt)
+    txt = re.sub(r"dna", "DNA", txt)
+    txt = re.sub(r"III", "3", txt)
+    txt = re.sub(r"the US", "America", txt)
+    txt = re.sub(r"Astrology", "astrology", txt)
+    txt = re.sub(r"Method", "method", txt)
+    txt = re.sub(r"Find", "find", txt)
+    txt = re.sub(r"banglore", "Banglore", txt)
+    txt = re.sub(r" J K ", " JK ", txt)
+    txt = re.sub(r"comfy", "comfortable", txt)
+    txt = re.sub(r"colour", "color", txt)
+    txt = re.sub(r"travellers", "travelers", txt)
+
+    # Remove urls and emails
+    txt = re.sub(r'^https?:\/\/.*[\r\n]*', ' ', txt, flags=re.MULTILINE)
+    txt = re.sub(r'[\w\.-]+@[\w\.-]+', ' ', txt, flags=re.MULTILINE)
+
+    # Replace words like sooooooo with so
+    txt = ''.join(''.join(s)[:2] for _, s in itertools.groupby(txt))
+
+    # Remove all symbols
+    txt = re.sub(r'[^A-Za-z\s]', r' ', txt)
+    txt = re.sub(r'\n', r' ', txt)
+
+    if lowercase:
+        txt = " ".join([w.lower() for w in txt.split()])
+
+    if remove_stops:
+        txt = " ".join([w for w in txt.split() if w not in stop_words])
+
+    wordnet_lemmatizer = WordNetLemmatizer()
+    txt = " ".join([wordnet_lemmatizer.lemmatize(w, pos='v') for w in txt.split()])
+    return txt
+
+
+def train_model():
+    train = pd.read_csv("train.csv")
+    test = pd.read_csv("test.csv")
+    train = train.sample(frac=1)
+    test = test.sample(frac=1)
+
+    list_sentences_train = train["comment_text"].fillna("CVxTz").values
+    list_sentences_train = np.concatenate((list_sentences_train, test["comment_text"].fillna("CVxTz").values))
+    sentences = []
+    max_size = len(list_sentences_train)
+
+    print('start', max_size)
+    for index_s, s in tqdm(enumerate(list_sentences_train), total=max_size):
+        sentences.append([x for x in clean_data(s).split() if x])
+
+    model = gensim.models.Word2Vec(sentences, size=300, window=5, min_count=2, workers=4)
+    model.save('w2v.model')
+    print(model.wv.most_similar(positive=['tell']))
+
+
+def test_model():
+    word2vec = gensim.models.Word2Vec.load('w2v.model')
+    print(len(word2vec.wv.vocab))
+    for word in ['make', 'test', 'speech']:
+        if word in word2vec.wv.vocab:
+            print(word, word2vec.wv[word])
+
